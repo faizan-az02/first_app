61f29bc96df372113b0c12f3af36a831006912e9
Faizan Aziz <faizan.professional02@gmail.com>
2025-08-03 20:09:38 +0500

    Scaffold, Buttons, Main UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,13 @@ void main() {
         title: Text("First App"),
         centerTitle: true,
       ),
+      body: Center(
+        child: Text("Testing Center")
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text('Press'),
+      ),
     ),
   ));
 }
