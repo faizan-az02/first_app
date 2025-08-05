977f5399c5e8cd4c456b4257a5060615478fc6d1
Faizan Aziz <faizan.professional02@gmail.com>
2025-08-05 14:16:01 +0500

    Assets Images

--- lib/main.dart
+++ lib/main.dart
@@ -32,9 +32,9 @@ class Home extends StatelessWidget {
         color: const Color.fromARGB(255, 223, 64, 255),
         child: Center(
           child: Image(
-            image: NetworkImage(
-              "https://images.unsplash.com/photo-1505506874110-6a7a69069a08?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            )
+            image: AssetImage(
+              'assets/Galaxy.jpg'
+            ),
           )
         )),
 
