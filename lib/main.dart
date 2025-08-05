57d9305601b99aa15cea102679e3de53a77b98f4
Faizan Aziz <faizan.professional02@gmail.com>
2025-08-05 21:27:35 +0500

    Restructuring the File

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+  runApp(MaterialApp(home: Home()));
 }
 
 class Home extends StatelessWidget {
-  
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       backgroundColor: const Color.fromRGBO(209, 186, 248, 1),
 
       appBar: AppBar(
@@ -26,15 +22,14 @@ class Home extends StatelessWidget {
             fontFamily: "Livvic",
             letterSpacing: 2,
             color: Colors.white,
-          )
           ),
+        ),
         centerTitle: true,
       ),
 
       body: Padding(
         padding: EdgeInsetsGeometry.symmetric(vertical: 70, horizontal: 30),
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Center(
@@ -44,7 +39,7 @@ class Home extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 15,),
+            SizedBox(height: 15),
 
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -65,7 +60,7 @@ class Home extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 15,),
+            SizedBox(height: 15),
 
             Text(
               'NAME',
@@ -76,7 +71,7 @@ class Home extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 5,),
+            SizedBox(height: 5),
 
             Text(
               'Faizan',
@@ -88,7 +83,7 @@ class Home extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 30,),
+            SizedBox(height: 30),
 
             Text(
               'CLASS',
@@ -99,7 +94,7 @@ class Home extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 5,),
+            SizedBox(height: 5),
 
             Text(
               'BS DS',
@@ -111,42 +106,41 @@ class Home extends StatelessWidget {
               ),
             ),
 
-            SizedBox(height: 50,),
+            SizedBox(height: 50),
 
             Column(
               children: [
-
                 Row(
                   children: [
                     Icon(
                       Icons.email,
                       color: const Color.fromARGB(255, 108, 33, 238),
-                      ),
+                    ),
 
                     Padding(
                       padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
                       child: Text(
                         "i221946@nu.edu.pk",
-                      style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      letterSpacing: 1,
-                      fontWeight: FontWeight.bold
-                          ),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          letterSpacing: 1,
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
+                    ),
                   ],
                 ),
 
-                SizedBox(height: 10,),
+                SizedBox(height: 10),
 
                 Row(
                   children: [
                     Icon(
-                    Icons.phone,
+                      Icons.phone,
                       color: const Color.fromARGB(255, 108, 33, 238),
                     ),
-                    
+
                     Padding(
                       padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
                       child: Text(
@@ -155,22 +149,22 @@ class Home extends StatelessWidget {
                           color: Colors.white,
                           fontSize: 20,
                           letterSpacing: 1,
-                          fontWeight: FontWeight.bold
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ],
                 ),
 
-                SizedBox(height: 10,),
+                SizedBox(height: 10),
 
                 Row(
                   children: [
                     Icon(
-                    Icons.home,
+                      Icons.home,
                       color: const Color.fromARGB(255, 108, 33, 238),
                     ),
-                    
+
                     Padding(
                       padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
                       child: Text(
@@ -179,17 +173,17 @@ class Home extends StatelessWidget {
                           color: Colors.white,
                           fontSize: 20,
                           letterSpacing: 1,
-                          fontWeight: FontWeight.bold
+                          fontWeight: FontWeight.bold,
                         ),
                       ),
                     ),
                   ],
                 ),
               ],
-              )
+            ),
           ],
-          )
-        )
+        ),
+      ),
     );
   }
 }
